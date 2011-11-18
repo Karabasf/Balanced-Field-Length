b374168570ddadd741a58d05f2c4bf9b123fa413
Dennis Tang <karabasf@gmail.com>
2011-11-18 14:42:13 +0100

    An exception derived class file was made
    Reason:
    - Gives a clearer exception when one of the mandatory methods is not implmenten from the base abstract class

---
+++ AircraftMethods/AircraftMethods/ImplementMethodException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AircraftMethods
+{
+    class ImplementMethodException:Exception
+    {
+        public ImplementMethodException() :base()
+        {
+        }
+
+        public ImplementMethodException(string message)
+            : base(message)
+        {
+
+        }
+    }
+}
