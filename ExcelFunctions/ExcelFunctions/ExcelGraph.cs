596921a8a2c7586bd72fba3043888b901187b523
Dennis Tang <karabasf@gmail.com>
2011-12-22 12:38:56 +0100

    Made two seperate classes in order to draw excelgraphs
    Actions taken:
    - base class Excelgraph was made. All the other graph classes should be derived from this oen
    - Line graph class was made. This was don in order to draw an line graph

---
+++ ExcelFunctions/ExcelFunctions/ExcelGraph.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelGenerator
+{
+    /// <summary>
+    /// Baseclass for each derived excelgraph objects
+    /// </summary>
+    abstract class ExcelGraph
+    {
+        private Excel.ChartObjects ExcelChartObjects;
+        protected Excel.Chart ExcelChart;
+
+        /// <summary>
+        /// Base class for each excel graph. Default
+        /// </summary>
+        /// <param name="targetSheet">The sheet where the graph should be put on</param>
+        /// <param name="xLocation">The X Location (from the left) of the graph</param>
+        /// <param name="yLocation">The Y location (from the top) of the graph</param>
+        /// <param name="xSize">The width of the graph</param>
+        /// <param name="ySize">The height of the graph</param>
+        protected virtual ExcelGraph(Excel._Worksheet targetSheet, double xLocation, double yLocation, 
+            double xSize, double ySize)
+        {
+            this.ExcelChartObjects = (Excel.ChartObjects)targetSheet.ChartObjects(Missing.Value);
+            this.ExcelChart = ExcelChartObjects.Add(xLocation, yLocation, xSize, ySize).Chart;
+        }
+
+        /// <summary>
+        /// General method to set the title of a graph
+        /// </summary>
+        /// <param name="ChartTitle">The title of the graph</param>
+        protected sealed virtual void setChartTitle(string ChartTitle)
+        {
+            ExcelChart.HasTitle = true;
+            ExcelChart.ChartTitle.Text = ChartTitle;
+        }
+
+        protected sealed virtual void implementMethod()
+        {
+        }
+    }
+}
---
+++ ExcelFunctions/ExcelFunctions/ExcelLineGraph.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelGenerator
+{
+    sealed class ExcelLineGraph : ExcelGraph
+    {
+        public ExcelLineGraph(Excel._Worksheet targetSheet, double xLocation, double yLocation,
+            double xSize, double ySize) : base(targetSheet, xLocation, yLocation, xSize, ySize)
+        {
+            ExcelChart.ChartType = Excel.XlChartType.xlLine;
+        }
+
+        /// <summary>
+        /// Method to set the title of the Excel line graph
+        /// </summary>
+        /// <param name="titleName">The title of the graph</param>
+        public void setGraphTitle(string titleName)
+        {
+            base.setChartTitle(titleName);
+        }
+    }
+}
