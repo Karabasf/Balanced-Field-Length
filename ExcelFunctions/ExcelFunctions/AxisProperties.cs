2cc9e59a02b4057a458cc9dc0c03af78804fd887
Dennis Tang <karabasf@gmail.com>
2011-12-22 13:29:40 +0100

    Made an additional class to hold the axis properties
    Reason:
    - Standardises axis when necessary

---
+++ ExcelFunctions/ExcelFunctions/AxisProperties.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExcelGenerator
+{
+    /// <summary>
+    /// A readonly class to hold relevant axis parameters
+    /// </summary>
+    class AxisProperties
+    {
+
+        public AxisProperties(string AxisName, int LabelSpacing, int TickMarkSpacing,
+            bool MajorGridlines, bool MinorGridlines)
+        {
+
+        }
+    }
+}
